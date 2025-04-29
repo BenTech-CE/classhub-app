e857dcd90c094846d47a663e3f6a3062bb902218
Ismael Nascimento <72841488+ismaelnascimento@users.noreply.github.com>
2025-04-29 14:13:59 -0300

    Update api.dart baseUrl

--- lib/core/utils/api.dart
+++ lib/core/utils/api.dart
@@ -1,4 +1,4 @@
 class Api {
-  static const String baseUrl = 'https://api.example.com';
+  static const String baseUrl = 'https://classhub-api.onrender.com';
   static const String loginEndpoint = '/api/auth/login';
 }
