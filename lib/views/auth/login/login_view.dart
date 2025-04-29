154d2cbd52fe767dd269eb0aaf901d0a8619976e
Ismael Nascimento <72841488+ismaelnascimento@users.noreply.github.com>
2025-04-29 18:57:15 -0300

    Função de registrar usuário e depois pegar suas informações

--- lib/views/auth/login/login_view.dart
+++ lib/views/auth/login/login_view.dart
@@ -50,97 +50,96 @@ class _LoginViewState extends State<LoginView> {
     final loading = authViewModel.isLoading;
 
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: SingleChildScrollView(
-        child: Container(
-          height: height,
-          padding: const EdgeInsets.fromLTRB(
-              sPadding, sPadding + 55, sPadding, sPadding),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const CircleAvatar(
-                  backgroundImage: AssetImage("assets/logo/logo.jpeg"),
-                  radius: 60),
-              const SizedBox(height: 24),
-              Text(
-                "Bem-vindo(a)\nde volta!",
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge
-                    ?.copyWith(fontSize: 40),
-              ),
-              const SizedBox(height: 50),
-              Text(
-                "E-mail:",
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              const TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Digite seu e-mail..."),
-              ),
-              const SizedBox(height: 12),
-              Text(
-                "Senha:",
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              const TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Digite sua senha..."),
-              ),
-              SizedBox(
-                width: double.maxFinite,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
+          child: Container(
+            height: height,
+            padding: const EdgeInsets.fromLTRB(
+                sPadding, sPadding + 55, sPadding, sPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                    backgroundImage: AssetImage("assets/logo/logo.jpeg"),
+                    radius: 60),
+                const SizedBox(height: 24),
+                Text(
+                  "Bem-vindo(a)\nde volta!",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineLarge
+                      ?.copyWith(fontSize: 40),
+                ),
+                const SizedBox(height: 50),
+                Text(
+                  "E-mail:",
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Digite seu e-mail..."),
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  "Senha:",
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Digite sua senha..."),
+                ),
+                SizedBox(
+                  width: double.maxFinite,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      TextButton(
+                          onPressed: () {},
+                          child: Text("Esqueceu a senha?",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.copyWith(color: cColorSecond)))
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 24),
+                SizedBox(
+                  width: double.maxFinite,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () => login(context),
+                    child: loading
+                        ? const SizedBox(
+                            width: 20,
+                            height: 20,
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                              strokeWidth: 1.5,
+                            ))
+                        : const Text("Entrar"),
+                  ),
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    Text("Não tem uma conta?",
+                        style: Theme.of(context).textTheme.bodyLarge),
                     TextButton(
                         onPressed: () {},
-                        child: Text("Esqueceu a senha?",
+                        child: Text("Registre-se",
                             style: Theme.of(context)
                                 .textTheme
                                 .bodyLarge
                                 ?.copyWith(color: cColorSecond)))
                   ],
-                ),
-              ),
-              const SizedBox(height: 24),
-              SizedBox(
-                width: double.maxFinite,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () => login(context),
-                  child: loading
-                      ? const SizedBox(
-                          width: 20,
-                          height: 20,
-                          child: CircularProgressIndicator(
-                            color: Colors.white,
-                            strokeWidth: 1.5,
-                          ))
-                      : const Text("Entrar"),
-                ),
-              ),
-              const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Não tem uma conta?",
-                      style: Theme.of(context).textTheme.bodyLarge),
-                  TextButton(
-                      onPressed: () {},
-                      child: Text("Registre-se",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyLarge
-                              ?.copyWith(color: cColorSecond)))
-                ],
-              )
-            ],
+                )
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
