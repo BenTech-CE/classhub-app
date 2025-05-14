37ddf6c6f9367b3f070b2a5d143fc982b44a2150
Kauã Sousa <kaua.sousa63@aluno.ifce.edu.br>
2025-05-14 12:17:56 -0300

    Update minimal class model

--- lib/models/class/management/minimal_class_model.dart
+++ lib/models/class/management/minimal_class_model.dart
@@ -3,12 +3,14 @@ class MinimalClassModel {
   final String name;
   final int color;
   final String school;
+  final bool canEdit;
 
   MinimalClassModel({
     required this.id,
     required this.name,
     required this.color,
-    required this.school
+    required this.school,
+    required this.canEdit
   });
 
   factory MinimalClassModel.fromJson(Map<String, dynamic> json) {
@@ -16,7 +18,8 @@ class MinimalClassModel {
       id: json['id'],
       name: json['name'],
       color: json['color'],
-      school: json['school']
+      school: json['school'],
+      canEdit: json['can_edit']
     );
   }
 }
