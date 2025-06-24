9ba560554194a7bf1fd2e14327e00517434059d8
Kauã Sousa <kaua.sousa63@aluno.ifce.edu.br>
2025-06-23 22:28:32 -0300

    Começando o CalendarView

--- lib/views/classes/routes/class_calendar_view.dart
+++ lib/views/classes/routes/class_calendar_view.dart
@@ -1,4 +1,7 @@
+import 'package:classhub/core/theme/colors.dart';
+import 'package:classhub/core/theme/sizes.dart';
 import 'package:classhub/models/class/management/minimal_class_model.dart';
+import 'package:classhub/widgets/ui/weekly_date_picker.dart';
 import 'package:flutter/material.dart';
 
 class ClassCalendarView extends StatefulWidget {
@@ -11,11 +14,37 @@ class ClassCalendarView extends StatefulWidget {
 }
 
 class _ClassCalendarViewState extends State<ClassCalendarView> {
+  DateTime _selectedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Calendar view")
+        WeeklyDatePicker(
+          selectedDay: _selectedDay,
+          changeDay: (value) => setState(() {
+            _selectedDay = value;
+          }),
+          enableWeeknumberText: false,
+          backgroundColor: Colors.transparent,
+          selectedDigitBackgroundColor: cColorTextAzul,
+          selectedDigitColor: cColorTextWhite,
+          weekdayTextColor: Colors.black54,
+          digitsColor: Colors.black54,
+          weekdays: const ["Seg", "Ter", "Qua", "Qui", "Sex", "Sáb", "Dom"],
+          daysInWeek: 7,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8), 
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Segunda-feira, 16 de junho")
+            ],
+          ),
+        )
+        
       ],
     );
   }
